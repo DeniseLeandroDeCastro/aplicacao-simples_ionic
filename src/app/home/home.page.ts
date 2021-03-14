6979e79a2c27bb22ad5db99a6c9de8be19568d49
Denise <dmaria.natal@gmail.com>
2021-03-14 10:12:38 -0300

    Exercício 1 - disciplina de dispositivos móveis e sem fio

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PickerController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  [x: string]: any;
 
-  constructor() {}
+  constructor(public _pickerCtrl:PickerController) {}
 
+  ngOnInit() {}
+
+  /**
+   * Função para exibir os detalhes
+   * do card na mesma página, através
+   * de um recurso chamado Picker.
+   */
+  async mostrarPickerBasico() {
+    const picker = await this._pickerCtrl.create({
+      columns: [
+        {
+          name:'Livraria Universitária',
+          options:[
+            {text: 'Livro1: A Casa Torta', value: 'livro1'},
+            {text: 'Livro2: Um Pressentimento Funesto', value: 'livro2'},
+            {text: 'Livro3: Assassinato na Casa do Pastor', value: 'livro3'},
+            {text: 'Idioma: Português', value: 'português'},
+            {text: 'R$ 164,84', value: 'preço'}
+          ]
+        }
+      ],
+      buttons: [
+        {
+          text: 'Exibir',
+          handler: (value) => {
+            console.log('Exibindo:', 'Coleção Agatha Christie');
+          }   
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: (value) => {
+            console.log('Cancelado', value);
+          }
+        }
+      ]
+    });
+    await picker.present();
+  }
 }
